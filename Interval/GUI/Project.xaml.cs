afdcfc93b84ba84abeb8501a4a57b1f8ea04671f
Eike Dreyer <eikedreyer@gmx.de>
2019-05-27 12:41:03 +0200

    init

---
+++ Interval/GUI/Project.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Interval.Logic.Project;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Die Elementvorlage "Benutzersteuerelement" wird unter https://go.microsoft.com/fwlink/?LinkId=234236 dokumentiert.
+
+namespace Interval.GUI
+{
+    public sealed partial class Project : UserControl
+    {
+        private Timer timer = new Timer();
+
+        public Project()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_start_Click(object sender, RoutedEventArgs e)
+        {
+            if(!timer.running)
+            {
+                timer.DispatcherStart(timer_text);
+            } else
+            {
+                timer.DispatcherStop();
+            }
+        }
+
+        private void showProjectDetails(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("test");
+        }
+
+    }
+}
---
+++ Interval/Logic/Project/Timer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Interval.Logic.Project
+{
+    class Timer
+    {
+        private DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        public int seconds { get; set; } = 0;
+        private TextBox timer_text;
+        public Boolean running { get; set; } = false;
+
+
+        public void DispatcherStart(TextBox timer_text)
+        {
+            this.timer_text = timer_text;
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+            running = true;
+        }
+
+        public void DispatcherStop()
+        {
+            dispatcherTimer.Stop();
+            running = false;
+        }
+
+        void dispatcherTimer_Tick(object sender, object e)
+        {
+            seconds += 1;
+            timer_text.Text = StringTimeFormatter();
+        }
+
+        private String StringTimeFormatter()
+        {
+            String hourZero = "";
+            String minZero = "";
+            String secZero = "";
+            if (getHours() <= 9) hourZero = "0";
+            if (getMinutes() <= 9) minZero = "0";
+            if (seconds <= 9) secZero = "0";
+
+            String strTime = hourZero + getHours() + ":" + minZero + getMinutes().ToString() + ":" + secZero + seconds.ToString();
+
+            return strTime;
+        }
+
+        private int getHours()
+        {
+            return seconds / 3600;
+        }
+
+        private int getMinutes()
+        {
+            return seconds / 60;
+        }
+
+    }
+    }
